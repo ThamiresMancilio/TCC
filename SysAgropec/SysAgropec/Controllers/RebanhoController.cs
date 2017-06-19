d4721cbb7ec41b5ecbb29a3bf2ee79f85c0efcd0
Thamires Mancílio <thamiresmanciliotec@gmail.com>
2017-06-19 15:44:24 -0300

    Dashboard e Controllers, Entidades e Enums

---
+++ SysAgropec/SysAgropec/Controllers/RebanhoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SysAgropec.Controllers
+{
+    public class RebanhoController : Controller
+    {
+        // GET: Rebanho
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        // GET: Rebanho
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
